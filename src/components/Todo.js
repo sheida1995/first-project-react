92ece6206adfe9acc2cb6d9d7280ce43b6ab2f73
sheidaarvan <sheidaarvan1@gmail.com>
2023-10-12 22:18:45 +0330

    last version of todolist

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,76 +1,87 @@
-import React, { useState } from 'react'
-import "../App.css"
+import React, { useState } from "react";
+import "../App.css";
 
 const Todo = () => {
-    const [inputData,setInputData]= useState('');
-    const [items, setItems]=useState([]);
+  const [inputData, setInputData] = useState("");
+  const [items, setItems] = useState([]);
 
-
-    const addItem = () => {
-      if (!inputData){
-
-      }else {
-        setItems([...items, inputData]);
-        setInputData('')
-      } 
+  const addItem = () => {
+    if (!inputData) {
+    } else {
+      setItems([...items, inputData]);
+      setInputData("");
     }
-    const deleteItem = () => {
-      const updatedItems = items.filter((elem , ind) => {
-        return 
+  };
+  const copyItems = [...addItem];
+  copyItems.splice();
+  const deleteItem = (id) => {
 
-      });
-      setItems(updatedItems);
-    }
+    const updatedItems = items.filter((elem , ind) => {
+    })
+  
 
+  const checkedItem = () => {
+    console.log("ok")
+  }
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
 
-    const submitHandler =(e) =>{
-      e.preventDefault();
-    }
-    
   return (
     <>
-    <h1>TODO LIST</h1>
-    <section className="container">
-      <div className="form-container">
-        <form className="todo-form" onSubmit={submitHandler}>
-          <input type="text" placeholder='Add todo' className="todo-input" value={inputData}
-          onChange={(e)=>setInputData(e.target.value)} />
-          <button className="todo-button" type="submit">
-            <i className="fa-solid fa-square-plus" title='add' onClick={addItem}></i>
-          </button>
-        </form>
+      <h1>TODO LIST</h1>
+      <section className="container">
+        <div className="form-container">
+          <form className="todo-form" onSubmit={submitHandler}>
+            <input
+              type="text"
+              placeholder="Add todo"
+              className="todo-input"
+              value={inputData}
+              onChange={(e) => setInputData(e.target.value)}
+            />
+            <button className="todo-button" type="submit"> onClick={addItem}
+              <i
+                className="fa-solid fa-square-plus"
+                title="add"
+                
+              ></i>
+            </button>
+          </form>
 
-        <div className="select">
-          <select className="filter-todos" name="todos">
-            <option value="all">All</option>
-            <option value="completed">Completed</option>
-            <option value="uncompleted">Uncompleted</option>
-          </select>
+          <div className="select">
+            <select className="filter-todos" name="todos">
+              <option value="all">All</option>
+              <option value="completed">Completed</option>
+              <option value="uncompleted">Uncompleted</option>
+            </select>
+          </div>
         </div>
-      </div>
-      <div className="todo-container">
-        {
-          items.map((elem, ind)=>{
-            return(
-<ul className="todo-list" key={ind}>
-        <li className="todo">
-        <p className="todo-title">{elem}</p>
-        <button className="check-button"><i className="fa-sharp fa-solid fa-check" ></i></button>
-        <button className="trash-button"><i className="fa-solid fa-trash" onClick={
-          () => deleteItem(ind)}></i></button>
-      </li>
-      </ul>
-            )
-          })
-
-        }
-        
-        
+        <div className="todo-container">
+          {items.map((elem, ind) => {
+            return (
+              <ul className="todo-list" key={ind}>
+                <li className="todo">
+                  <p className="todo-title">{elem}</p>
+                  <button className="check-button" onClick={checkedItem}>
+                    <i
+                      className="fa-sharp fa-solid fa-check"
+                      
+                    ></i>
+                  </button>
+                  <button className="trash-button">
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </li>
+              </ul>
+            );
+          })}
         </div>
       </section>
     </>
-  )
+  );
+};
 }
 
-export default Todo
+export default Todo;
